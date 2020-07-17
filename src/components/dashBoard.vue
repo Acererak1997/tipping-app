bf29783044e457bc641cb9b5fd78a367f630ac47
Cthulhu <jaxa5963@gmail.com>
2020-07-17 17:32:29 +0900

    Vueタスク4-4.1

--- src/components/dashBoard.vue
+++ src/components/dashBoard.vue
@@ -3,15 +3,29 @@
     <p>{{ user.data.displayName }}さん、ようこそ！</p>
     <p>残高： {{ user.wallet }}円</p>
     <h1>ユーザー一覧</h1>
+    <button @click="logOut">ログアウト</button>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import firebase from "firebase";
 
 export default {
   computed: {
     ...mapGetters({ user: "user" })
+  },
+  methods: {
+    logOut() {
+      firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        this.$router.replace({
+          name: "Signin"
+        })
+      })
+    }
   }
 };
 </script>
